52210dc0d9db8b313977c386586a8e41b5839aba
Wu Shiguang <1543159426@qq.com>
2021-06-09 01:31:15 +0800

    add sleep.c user prog

---
+++ user/sleep.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) exit(-1);
+
+    exit(sleep(atoi(argv[1])));
+}
