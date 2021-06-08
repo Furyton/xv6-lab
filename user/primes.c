dbe0586c291ccd6168a3c39ea1ee40314b0e941e
Wu Shiguang <1543159426@qq.com>
2021-06-09 03:31:46 +0800

    finish primes.c

--- user/primes.c
+++ user/primes.c
@@ -2,35 +2,55 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+void pipeline(int fd[]) {
+    int p, n, cfd[2] = {-1};
+    close(fd[1]);
+    if (read(fd[0], &p, sizeof(int)) > 0) {
+        printf("prime: %d\n", p);
+
+        while(read(fd[0], &n, sizeof(int)) > 0) {
+            if (n % p != 0) {
+                if (cfd[0] < 0) {
+                    pipe(cfd);
+                    int pid = fork();
+
+                    if (pid == 0) {
+                        pipeline(cfd);
+                        exit(0);
+                    }
+                }
+
+                write(cfd[1], &n, sizeof(int));
+            }
+        }
+
+        close(cfd[1]);
+        wait(0);
+        close(cfd[0]);
+        close(cfd[1]);
+    }
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
-    int pid, p[2];
-    pipe(p);
-    pid = fork();
-    
+    int pid, pfd[2];
+    pipe(pfd);
 
-    int data[5] = {1, 2, 3, 4, 5}, buf[5];
+    pid = fork();
 
     if(pid == 0) {
-        close(p[0]);
-        if(read(p[0], &buf, sizeof(int)) > 0) {
-            printf("1,received %d\n", buf[0]);
-        }
-        if(read(p[0], &buf, sizeof(int)) > 0) {
-            printf("2,received %d\n", buf[0]);
-        }
-        if(read(p[0], &buf, sizeof(int)) > 0) {
-            printf("3,received %d\n", buf[0]);
-        }
-        exit(0);
+        pipeline(pfd);
     } else {
-        write(p[1], &data[0], sizeof(int));
-        write(p[1], &data[1], sizeof(int));
-        close(p[1]);
-        close(p[0]);
+        for(int i = 2; i <= 35; i++) {
+            write(pfd[1], &i, sizeof(int));
+        }
+        
+        close(pfd[1]);
+
         wait(0);
 
-        close(p[0]);
+        close(pfd[0]);
     }
 
     exit(0);
