3a3c42438bdcfe22588b7a058f0740b830e90421
Furyton <1543159426@qq.com>
2021-07-31 21:46:56 +0800

    modified trap

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,8 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  // if(growproc(n) < 0)
+  //   return -1;
   return addr;
 }
 
--- kernel/vm.c
+++ kernel/vm.c
@@ -182,8 +182,8 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
       panic("uvmunmap: walk");
-    if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+    // if((*pte & PTE_V) == 0)
+      // panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
